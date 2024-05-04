316f214dedc0b99f7e4387868a09d9ec28f02317
Muhammad Aliff Bin Muhamad Shukri <ai210270@student.uthm.edu.my>
2024-05-05 02:12:49 +0800

    tnc page and show password

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -18,107 +18,106 @@ class _LoginPageState extends State<LoginPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   String _role = ''; // Track selected role
+  bool _obscureText = true; // Track whether password text is obscured
 
   Future<void> logIn(BuildContext context) async {
-  if (_role.isEmpty) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Role not selected"),
-          content: Text("Please select your role."),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
-    return; // Exit the function if role not selected
-  }
-
-  showDialog(
-    context: context,
-    barrierDismissible: false,
-    builder: (BuildContext context) {
-      return Center(
-        child: CircularProgressIndicator(),
+    if (_role.isEmpty) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Role not selected"),
+            content: Text("Please select your role."),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
       );
-    },
-  );
-
-  try {
-    if (_role == 'Customer') {
-      // Check if entered credentials match admin credentials
-      String adminEmail = 'admin@carapp.com'; // Admin email
-      String adminPassword = 'admin@123'; // Admin password
-      String enteredEmail = _emailController.text.trim();
-      String enteredPassword = _passwordController.text.trim();
-      if (enteredEmail == adminEmail && enteredPassword == adminPassword) {
-        // If admin credentials used for customer role, show error message
-        throw Exception("Admin credentials cannot be used to log in as a customer.");
-      }
-      
-      // Perform customer login using Firebase
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: enteredEmail, 
-        password: enteredPassword,
-      );
-    } else if (_role == 'Admin') {
-      // Check if the entered credentials are for admin
-      String adminEmail = 'admin@carapp.com'; // Change to your admin email
-      String adminPassword = 'admin@123'; // Change to your admin password
-      String enteredEmail = _emailController.text.trim();
-      String enteredPassword = _passwordController.text.trim();
-      if (enteredEmail == adminEmail && enteredPassword == adminPassword) {
-        // Navigate to StaffCarRentalPage after successful admin login
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => StaffCarRentalPage()),
-        );
-        return; // Exit the function after navigation
-      } else {
-        // If selected role is Admin but entered credentials don't match admin credentials
-        throw Exception("Incorrect admin credentials");
-      }
+      return; // Exit the function if role not selected
     }
 
-    Navigator.of(context, rootNavigator: true).pop();
-
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => HomePage()),
-    );
-  } catch (e) {
-    Navigator.of(context, rootNavigator: true).pop();
-
-    print("Login failed: $e");
-    
     showDialog(
       context: context,
+      barrierDismissible: false,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Login Error"),
-          content: Text("User not found or incorrect credentials. Please try again."),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('OK'),
-            ),
-          ],
+        return Center(
+          child: CircularProgressIndicator(),
         );
       },
     );
+
+    try {
+      if (_role == 'Customer') {
+        // Check if entered credentials match admin credentials
+        String adminEmail = 'admin@carapp.com'; // Admin email
+        String adminPassword = 'admin@123'; // Admin password
+        String enteredEmail = _emailController.text.trim();
+        String enteredPassword = _passwordController.text.trim();
+        if (enteredEmail == adminEmail && enteredPassword == adminPassword) {
+          // If admin credentials used for customer role, show error message
+          throw Exception("Admin credentials cannot be used to log in as a customer.");
+        }
+        
+        // Perform customer login using Firebase
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: enteredEmail, 
+          password: enteredPassword,
+        );
+      } else if (_role == 'Admin') {
+        // Check if the entered credentials are for admin
+        String adminEmail = 'admin@carapp.com'; // Change to your admin email
+        String adminPassword = 'admin@123'; // Change to your admin password
+        String enteredEmail = _emailController.text.trim();
+        String enteredPassword = _passwordController.text.trim();
+        if (enteredEmail == adminEmail && enteredPassword == adminPassword) {
+          // Navigate to StaffCarRentalPage after successful admin login
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => StaffCarRentalPage()),
+          );
+          return; // Exit the function after navigation
+        } else {
+          // If selected role is Admin but entered credentials don't match admin credentials
+          throw Exception("Incorrect admin credentials");
+        }
+      }
+
+      Navigator.of(context, rootNavigator: true).pop();
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    } catch (e) {
+      Navigator.of(context, rootNavigator: true).pop();
+
+      print("Login failed: $e");
+      
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Login Error"),
+            content: Text("User not found or incorrect credentials. Please try again."),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
-}
-
-
 
   @override
   void dispose(){
@@ -185,10 +184,20 @@ class _LoginPageState extends State<LoginPage> {
                       padding: const EdgeInsets.only(left: 20.0),
                       child: TextField(
                         controller: _passwordController,
-                        obscureText: true,
+                        obscureText: _obscureText,
                         decoration: InputDecoration(
                           border: InputBorder.none,
                           hintText: 'Password',
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _obscureText = !_obscureText;
+                              });
+                            },
+                            child: Icon(
+                              _obscureText ? Icons.visibility : Icons.visibility_off,
+                            ),
+                          ),
                         ),
                       ),
                     ),
--- lib/pages/terms_page.dart
+++ lib/pages/terms_page.dart
@@ -1,15 +1,80 @@
 import 'package:flutter/material.dart';
 
-class TermsPage extends StatefulWidget {
-  const TermsPage({super.key});
+class TermsPage extends StatelessWidget {
+  const TermsPage({Key? key}) : super(key: key);
 
-  @override
-  State<TermsPage> createState() => _TermsPageState();
-}
-
-class _TermsPageState extends State<TermsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 178, 191, 83),
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // Remove the back arrow
+        centerTitle: true,
+        title: Text(
+          "Terms and Conditions",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: Color.fromARGB(255, 173, 129, 80),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white, // White background color for the container
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          padding: EdgeInsets.all(20.0), // Padding inside the container
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildTerm("1.", "The MINIMUM age for borrowers and additional drivers is 18 YEARS OLD and MUST have a valid and current local driver's license. Probationary driving license holders will not be accepted."),
+              _buildTerm("2.", "Customer need to update the fuel status after finishing their rental period."),
+              _buildTerm("3.", "The vehicle's fuel meter bar when received must be the same as when the vehicle is returned. Reducing the fuel meter bar from the original will incur an ADDITIONAL CHARGE. The cost of fuel is the responsibility of the customer, and it cannot be claimed."),
+              _buildTerm("4.", "The customer is responsible for paying all summonses, compounds, or fines throughout the rental period."),
+              _buildTerm("5.", "The customer is responsible for ensuring that the car is always in good condition and clean throughout the rental period until the vehicle is returned. Borrowers are absolutely FORBIDDEN to spit, smoke or litter in the car. If it is found that the exterior and interior of the car are very dirty such as spilled water or food on the car dashboard, door panels or cushions, and there is an unpleasant smell or there is any damage, the CUSTOMER will be charged an ADDITIONAL CHARGE for cleaning and all the costs of restoring the vehicle to condition original."),
+              _buildTerm("6.", "The customer rental car will be tracked during rental period by the staff for safety reasons."),
+              _buildTerm("7.", "If an accident occurs, the CUSTOMER must inform us as soon as possible and not take any other action before that to avoid problems between the borrower and us in the future."),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context); // Navigate back to the previous page
+          },
+          child: Text("Return to Register"),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTerm(String number, String text) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            number,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(width: 8),
+          Expanded(
+            child: Text(
+              text,
+              textAlign: TextAlign.justify,
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
